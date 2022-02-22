b6f83c5b0ff726d503fa70c5114e42e200e9879c
Niraj Paghadar <niraj.paghadar@prowerse.com>
2022-02-22 17:22:57 +0530

    1) Changes in settings file

--- RealestateApi/RealestateApi/settings.py
+++ RealestateApi/RealestateApi/settings.py
@@ -127,10 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
